ce7579c4851f0694d96a851bb8ff335f74c34b74
lemontreeran <ran.xing@gmail.com>
2024-12-04 11:03:27 +1100

    First commit

---
+++ background.js
@@ -0,0 +1,14 @@
+"use strict";
+
+console.log("connected...");
+let prompt = "";
+
+// On Message
+chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+    if (request.message === "setPrompt") {
+        prompt = request.prompt;
+    } else if (request.message === "getPrompt") {
+        sendResponse({ prompt: prompt });
+        prompt = ""; // Reset prompt
+    }
+});
